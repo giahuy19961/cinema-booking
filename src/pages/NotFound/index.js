0451bcd7881b0504925f4abf1a6a8b1bea15425d
giahuy19961 <70837177+giahuy19961@users.noreply.github.com>
2021-08-16 11:02:49 +0700

    Not found

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const PageNotFound = () => {
-  return <div className='d-flex justify-content-center'>404 NOT FOUND</div>;
+  return (
+    <div
+      className='d-flex justify-content-center align-items-center'
+      style={{ height: "100vh" }}
+    >
+      <img src='https://cdn.tgdd.vn/hoi-dap/580732/Thumbnail/cach-khac-phuc-loi-404-not-found-thumb-1.jpg' />
+    </div>
+  );
 };
 
 export default PageNotFound;
