16c6ba67fc53a964e5d1cca10867017b27439dc3
giahuy19961 <70837177+giahuy19961@users.noreply.github.com>
2021-10-17 13:53:16 +0700

    Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,47 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import {
   FooterContainer,
   FooterRow,
-  FooterContent,
   FooterWrap,
   Devider,
-  ContentHeader,
-  FooterMenu,
+  FooterTitle,
+  FooterCol,
   FooterItem,
-} from './styles';
+  FooterMenu,
+  FooterLogo,
+  FooterLogoList,
+  FooterCoLogo,
+  FooterRowUnder,
+} from "./styles";
+import LogoSrc from "../../assets/img/cyberlogo.png";
+import { useSelector } from "react-redux";
+import Loading from "components/Loading";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillTwitterCircle } from "react-icons/ai";
 
 const Footer = () => {
+  const { data, loading } = useSelector((state) => state.listTheatersReducer);
+
+  const renderCoopLogo = (list) => {
+    return list?.map((theater, index) => (
+      <FooterCoLogo key={index}>
+        <img src={theater.logo}></img>
+      </FooterCoLogo>
+    ));
+  };
+  if (loading) return <Loading />;
+
   return (
     <FooterWrap>
       <FooterContainer>
-        <FooterRow className="row">
-          <FooterContent className="col-4">
-            <ContentHeader>CyberCinema</ContentHeader>
+        <FooterRow>
+          <FooterCol>
+            <FooterLogo>
+              <img src={LogoSrc}></img>
+            </FooterLogo>
             <FooterMenu>
-              <FooterItem>FAQ</FooterItem>
-              <FooterItem>brand-guideline</FooterItem>
-              <FooterItem>Thỏa thuận sử dụng</FooterItem>
-              <FooterItem>Chính sách bảo mật</FooterItem>
+              <FooterItem>
+                <a href='#'>FAQ</a>
+              </FooterItem>
+              <FooterItem>
+                <a href='#'>Thỏa thuận sử dụng</a>
+              </FooterItem>
+              <FooterItem>
+                <a href='#'>Brand Guidelines</a>
+              </FooterItem>
+              <FooterItem>
+                <a href='#'>Chính sách bảo mật</a>
+              </FooterItem>
             </FooterMenu>
-          </FooterContent>
-          <FooterContent className="col-4">
-            <ContentHeader>Đối tác</ContentHeader>
-          </FooterContent>
-          <FooterContent className="col-4">
-            <ContentHeader>App & Social</ContentHeader>
-          </FooterContent>
+          </FooterCol>
+          <FooterCol>
+            <FooterTitle>Đối tác</FooterTitle>
+            <FooterLogoList>{renderCoopLogo(data)}</FooterLogoList>
+          </FooterCol>
+          <FooterCol>
+            <FooterTitle>Social</FooterTitle>
+            <FooterLogoList>
+              <FooterCoLogo>
+                <FaFacebook />
+              </FooterCoLogo>
+              <FooterCoLogo>
+                <AiFillTwitterCircle />
+              </FooterCoLogo>
+            </FooterLogoList>
+          </FooterCol>
         </FooterRow>
         <Devider />
-        <FooterRow className="row">
-          <FooterContent className="col-12">
-            <ContentHeader> CYBER_CINEMA – SẢN PHẨM CỦA GH</ContentHeader>
-            <p>
-              email:
-              <a href="mailto:giahuybk96@gmail.com">giahuybk96@gmail.com</a> for
-              more info
-            </p>
-          </FooterContent>
-        </FooterRow>
+        <FooterRowUnder>
+          <FooterTitle>Thanks for tix.vn template supported </FooterTitle>
+          <p>Last project completed learning at cybersoft</p>
+          <p>
+            Contact:
+            <a href='mailto:giahuybk96@gmail.com'>giahuybk96@gmail.com</a>
+          </p>
+        </FooterRowUnder>
       </FooterContainer>
     </FooterWrap>
   );
